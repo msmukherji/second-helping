4323dfe8274210f873f7ad820e116c2ebeae0e93
Mishka <mishka.s.mukherji@gmail.com>
2015-04-08 14:38:41 -0400

    twilio sent me a message

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -10,11 +10,29 @@ class Recipient < ActiveRecord::Base
     unless Claim.find_by(donation_id: donation.id) && Claim.find_by(donation_id: donation.id).approved? == true
       claim = Claim.create! donation_id: donation.id, recipient_id: self.id
       notify_claimed claim
+      send_text_message
     end
   end
 
   def notify_claimed claim
     mailer = RecipientMailer.alert_claim claim
     mailer.deliver_later
+  end
+
+  def send_text_message
+    number_to_send_to = "2406205793"#self.donation.donor.contact_number
+ 
+    twilio_sid = Figaro.env.twilio_account_sid
+    twilio_token = Figaro.env.twilio_auth_token
+    twilio_phone_number = "2402057648"
+    message = "hey the thing worked"
+ 
+    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+ 
+    @twilio_client.account.sms.messages.create(
+      :from => "+1#{twilio_phone_number}",
+      :to => number_to_send_to,
+      :body => message
+    )
   end
 end
